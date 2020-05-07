cbafed7243f5545b640cad04a17b28d634b538d4
unknown <shorser8@gmail.com>
2020-05-07 21:48:54 +0300

    <home work>-<restyle autorization, add validation logn in and sign up>

--- src/authorization.js
+++ src/authorization.js
@@ -6,12 +6,15 @@ const axios = require('axios').default;
 
 const Authorization = {
     Menu() {
+        const logIn = document.querySelector('.logIn')
         const logButton = document.querySelector('.titleLog')
         const signButton = document.querySelector('.titleSign')
 
         const logBody = document.querySelector('.bodyLogIn')
         const signbODY = document.querySelector('.bodySignUp')
 
+        logIn.style.top = document.documentElement.clientHeight / 2 - 250 + "px"
+        logIn.style.left = document.documentElement.clientWidth / 2 - 200 + "px"
 
 
         logButton.addEventListener('click', function () {
@@ -43,6 +46,7 @@ const Authorization = {
 
         username.closest('.username').addEventListener('click', function () {
             username.focus()
+            document.querySelector('.messageError').style.display = 'none'
             username.closest('.username').style.color = 'rgb(29,161,242)'
             username.closest('.username').style.borderBottom = 'solid 2px rgb(29,161,242)'
 
@@ -61,6 +65,7 @@ const Authorization = {
         })
         password.closest('.password').addEventListener('click', function () {
             password.focus()
+            document.querySelector('.messageError').style.display = 'none'
             password.closest('.password').style.color = 'rgb(29,161,242)'
             password.closest('.password').style.borderBottom = 'solid 2px rgb(29,161,242)'
 
@@ -91,8 +96,9 @@ const Authorization = {
                     .then(function (response) {
                         console.log('User log in!', response)
                         if (response.data === 'Login error') {
-                            document.querySelector('.messageError').style.display = 'block'
+                            document.querySelector('.logMessageError').style.display = 'block'
                         } else {
+                            document.querySelector('.logMessageError').style.display = 'none'
                             document.querySelector('.containerLogIn').style.display = 'none'
                             username.value = ""
                             password.value = ""
@@ -115,29 +121,41 @@ const Authorization = {
         const confNewPassword = document.querySelector('#passConfirm')
         const newEmail = document.querySelector('#newEmail')
         const submiteNewUser = document.querySelector('.buttonSingUp')
+        const messageError =document.querySelector('.signMessageError')
         let statusSignUp = false
-        let emailStatus = false
 
         newUsername.closest('.username').addEventListener('click', function () {
+            document.querySelector('.signMessageError').style.display = "none"
+            
             newUsername.focus()
             newUsername.closest('.username').style.color = 'rgb(29,161,242)'
             newUsername.closest('.username').style.borderBottom = 'solid 2px rgb(29,161,242)'
 
             newUsername.addEventListener('blur', function () {
-                newUsername.closest('.username').style.color = 'black'
-                newUsername.closest('.username').style.borderBottom = '2px solid rgb(101, 119, 134)'
+                var reg = /^([A-Za-z])+([A-Za-z0-9_\-\.])$/;
+                var name = newUsername.value
 
-                if (newUsername.value && newPassword.value && confNewPassword.value && emailStatus) {
-                    submiteNewUser.style.backgroundColor = 'rgb(29,161,242)'
-                    statusSignUp = true
+                if (reg.test(name) == false) {
+                    messageError.innerHTML = 'Username must be at least 2 characters long and begin with a letter'
+                    messageError.style.display = "block"
+                    newUsername.closest('.username').style.borderBottom = '2px solid rgb(300, 0, 0)'
                 } else {
-                    submiteNewUser.style.backgroundColor = 'rgba(29,161,242,0.5)'
-                    statusSignUp = false
+                    newUsername.closest('.username').style.color = 'black'
+                    newUsername.closest('.username').style.borderBottom = '2px solid rgb(101, 119, 134)'
+
+                    if (newUsername.value && newPassword.value && confNewPassword.value) {
+                        submiteNewUser.style.backgroundColor = 'rgb(29,161,242)'
+                        statusSignUp = true
+                    } else {
+                        submiteNewUser.style.backgroundColor = 'rgba(29,161,242,0.5)'
+                        statusSignUp = false
+                    }
                 }
             })
         })
 
         newPassword.closest('.password').addEventListener('click', function () {
+            messageError.style.display = "none"
             newPassword.focus()
             newPassword.closest('.password').style.color = 'rgb(29,161,242)'
             newPassword.closest('.password').style.borderBottom = 'solid 2px rgb(29,161,242)'
@@ -145,13 +163,15 @@ const Authorization = {
             newPassword.addEventListener('blur', function () {
                 newPassword.closest('.password').style.color = 'black'
 
-                if (newUsername.value && newPassword.value && confNewPassword.value && emailStatus) {
+                if (newUsername.value && newPassword.value && confNewPassword.value) {
                     if (newPassword.value === confNewPassword.value) {
                         newPassword.closest('.password').style.borderBottom = '2px solid rgb(101, 119, 134)'
                         confNewPassword.closest('.password').style.borderBottom = '2px solid rgb(101, 119, 134)'
                         submiteNewUser.style.backgroundColor = 'rgb(29,161,242)'
                         statusSignUp = true
                     } else {
+                        messageError.innerHTML = 'passwords must match'
+                        messageError.style.display = "block"
                         newPassword.closest('.password').style.borderBottom = '2px solid  rgb(300, 0, 0)'
                         statusSignUp = false
                         submiteNewUser.style.backgroundColor = 'rgba(29,161,242,0.5)'
@@ -164,6 +184,7 @@ const Authorization = {
         })
 
         confNewPassword.closest('.password').addEventListener('click', function () {
+            messageError.style.display = "none"
             confNewPassword.focus()
             confNewPassword.closest('.password').style.color = 'rgb(29,161,242)'
             confNewPassword.closest('.password').style.borderBottom = 'solid 2px rgb(29,161,242)'
@@ -172,12 +193,14 @@ const Authorization = {
                 confNewPassword.closest('.password').style.color = 'black'
 
 
-                if (newUsername.value && newPassword.value && confNewPassword.value && newEmail.value) {
+                if (newUsername.value && newPassword.value && confNewPassword.value) {
                     if (newPassword.value === confNewPassword.value) {
                         confNewPassword.closest('.password').style.borderBottom = '2px solid rgb(101, 119, 134)'
                         submiteNewUser.style.backgroundColor = 'rgb(29,161,242)'
                         statusSignUp = true
                     } else {
+                        messageError.innerHTML = 'passwords must match'
+                        messageError.style.display = "block"
                         confNewPassword.closest('.password').style.borderBottom = '2px solid  rgb(300, 0, 0)'
                         statusSignUp = false
                         submiteNewUser.style.backgroundColor = 'rgba(29,161,242,0.5)'
@@ -190,7 +213,7 @@ const Authorization = {
         })
 
         newEmail.closest('.email').addEventListener('click', function () {
-
+            messageError.style.display = "none"
             newEmail.focus()
             newEmail.closest('.email').style.color = 'rgb(29,161,242)'
             newEmail.closest('.email').style.borderBottom = 'solid 2px rgb(29,161,242)'
@@ -200,16 +223,15 @@ const Authorization = {
                 var address = newEmail.value
 
                 if (reg.test(address) == false) {
+                    messageError.innerHTML = 'Your email address is incorrect'
+                        messageError.style.display = "block"
                     newEmail.closest('.email').style.borderBottom = '2px solid rgb(300, 0, 0)'
-                    emailStatus = false
                 } else {
                     newEmail.closest('.email').style.color = 'black'
                     newEmail.closest('.email').style.borderBottom = '2px solid rgb(101, 119, 134)'
-                    emailStatus = true
                 }
 
-                if (newUsername.value && newPassword.value && confNewPassword.value && emailStatus) {
-                    console.log(emailStatus)
+                if (newUsername.value && newPassword.value && confNewPassword.value) {
                     submiteNewUser.style.backgroundColor = 'rgb(29,161,242)'
                     statusSignUp = true
                 } else {
@@ -252,7 +274,7 @@ const Authorization = {
         user.addEventListener('click', function () {
             logMenu.style.display = 'block'
             logMenu.focus()
-            
+
             logMenu.addEventListener('blur', () => {
                 logMenu.style.display = 'none'
             })
